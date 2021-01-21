781bacd4889d4f639090de45f1290ab2413c2b8c
Spitchenko Anatoly <anatolij.sp@yandex.ru>
2021-01-21 21:17:31 +0300

    Initial commit

---
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -0,0 +1,44 @@
+package ru.netology
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class MainKtTest {
+
+    @Test
+    fun calculatePrice_userNormalDiscount() {
+        val itemPrice = 100
+        val itemCount = 11
+        val discount = 100
+        val discountStart = 1_000
+        val expected = 1_000
+
+        val result = calculatePrice(
+            itemCount = itemCount,
+            itemPrice = itemPrice,
+            discount = discount,
+            discountStart = discountStart,
+        )
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun calculatePrice_userGoldDiscount() {
+        val itemPrice = 100
+        val itemCount = 11
+        val discount = 100
+        val discountStart = 1_000
+        val expected = 900
+
+        val result = calculatePrice(
+            userLevel = "gold",
+            itemCount = itemCount,
+            itemPrice = itemPrice,
+            discount = discount,
+            discountStart = discountStart,
+        )
+
+        assertEquals(expected, result)
+    }
+}
